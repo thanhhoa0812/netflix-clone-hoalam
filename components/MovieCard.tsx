d425fb442b76e929e07641413c7730c74e2fae5e
hoalam <thanhhoaafk@gmail.com>
2025-09-10 17:48:27 +0700

    update css list movie
    update css list movie

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,11 +1,23 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import type { TMDbMovie } from '@/types/tmdb';
+import Image from "next/image";
+import Link from "next/link";
+import type { TMDbMovie } from "@/types/tmdb";
 
 export default function MovieCard({ movie }: { movie: TMDbMovie }) {
-  const poster = movie.poster_path ? `https://image.tmdb.org/t/p/w342${movie.poster_path}` : undefined;
+  const poster = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+    : undefined;
+  const votePercent = Math.round(movie.vote_average * 10);
+  const getVoteColor = (percent: number) => {
+    if (percent >= 70) return "bg-emerald-500";
+    if (percent >= 50) return "bg-yellow-500";
+    return "bg-red-500";
+  };
   return (
-    <Link  href={{ pathname: `/${movie.id}` }} className="group w-36 md:w-44 lg:w-56 shrink-0" title={movie.title}>
+    <Link
+      href={{ pathname: `/${movie.id}` }}
+      className="group w-36 md:w-44 lg:w-56 shrink-0"
+      title={movie.title}
+    >
       <div className="relative aspect-[2/3] rounded-xl overflow-hidden bg-black/30">
         {poster && (
           <Image
@@ -18,8 +30,17 @@ export default function MovieCard({ movie }: { movie: TMDbMovie }) {
         )}
       </div>
       <div className="mt-2 space-y-1">
-        <p className="text-sm md:text-base font-medium line-clamp-1">{movie.title}</p>
-        <p className="text-xs text-white/60">⭐ ⭐ {movie.vote_average?.toFixed(1) ?? 'N/A'}</p>
+        <p className="text-sm md:text-base font-medium line-clamp-1">
+          {movie.title}
+        </p>
+        <div
+          className={`-top-2 -left-2 ${getVoteColor(
+            votePercent
+          )} text-white text-xs font-bold rounded-full w-10 h-7 flex items-center justify-center shadow-md`}
+        >
+          {votePercent}%
+        </div>
+        
       </div>
     </Link>
   );
--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -12,7 +12,7 @@ export default function MovieList({
     <section id="rows" className="px-4 md:px-10">
       <h2 className="text-xl md:text-2xl font-semibold mb-3">{title}</h2>
       <div className="relative">
-        <div className="flex gap-3 overflow-x-auto pb-4 scrollbar-hide snap-x snap-mandatory">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-10 gap-6 scrollbar-hide snap-x snap-mandatory scroll-smooth">
           {movies.map((m) => (
             <div key={m.id} className="snap-start">
               <MovieCard movie={m} />
